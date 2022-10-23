d7b658cee160727ebfcb3715870c537a69ae0685
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-10-23 18:46:29 +0530

    splash screen added with anims and transitions

---
+++ app/src/main/java/com/example/eadapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.eadapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.ActionBar;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView startupImg;
+    Animation fade;
+    private static int STARTUP_SCREEN = 6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        overridePendingTransition(
+                R.anim.enter_activity, R.anim.exit_activity);
+
+        // To hide status bar from the starting activity window
+        getWindow().getDecorView().getWindowInsetsController().hide(
+                android.view.WindowInsets.Type.statusBars()
+                        | android.view.WindowInsets.Type.navigationBars()
+        );
+
+        //Animations
+        fade = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        //Hooks
+        startupImg = findViewById(R.id.splash_screen_img);
+
+        //Assign Animations
+        startupImg.setAnimation(fade);
+
+        // Create an executor that executes tasks in a background thread.
+        ScheduledExecutorService backgroundExecutor = Executors.newSingleThreadScheduledExecutor();
+
+        // Execute a task in the background thread after 3 seconds.
+        backgroundExecutor.schedule(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, OwnerView.class);
+                startActivity(intent);
+                //To remove this activity from activity list we use finish() - now users cannot open startup screen by pressing bak button
+                finish();
+                overridePendingTransition(
+                        R.anim.enter_activity, R.anim.exit_activity);
+            }
+        }, STARTUP_SCREEN, TimeUnit.SECONDS);
+
+
+    }
+}
